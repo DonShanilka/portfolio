68257988b6436b210e7eff7f9e781798f57667a0
Shanilka <shanilka799@gmail.com>
2024-04-19 18:30:08 +0530

    home page circle js

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const text = document.querySelector('.text p');
+text.innerHTML = innerText.split('').map(
+ (char, i) =>
+  `<span style=""transform:rotate(${i * 8.3}deg)">${char}</span>`
+).join('');
