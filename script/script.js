4468ee22e6e4a7d0325770cc74a6fc5792fcf152
Shanilka <shanilka799@gmail.com>
2024-04-19 18:56:39 +0530

    add js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
 
+// Home page js add
+const text = document.querySelector('.text p');
+text.innerHTML = text.innerText.split("").map(
+ (char, i) =>
+  `<span style="transform:rotate(${i * 9.6}deg)">${char}</span>`
+).join("");
