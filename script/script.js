2a48c6e6a3e04004c250c079957948b5c169369b
Shanilka <shanilka799@gmail.com>
2024-04-19 18:27:21 +0530

    add home page circle js

---
+++ script/script.js
@@ -0,0 +1,6 @@
+
+const text = document.querySelector('.text p');
+text.innerHTML = innerText.split('').map(
+ (char, i) =>
+  `<span style=""transform:rotate(${i * 8.3}deg)">${char}</span>`
+).join('');
