48e2583b429233f92572269acb8fca060a3891b1
Shanilka <shanilka799@gmail.com>
2024-04-25 13:02:47 +0530

    add js in project page next back button

--- script/script.js
+++ script/script.js
@@ -12,7 +12,15 @@ let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
-scrollContainer.addEventListener("wheel", (evt) => {
-  evt.preventDefault();
-  scrollContainer.scrollLeft += evt.deltaY;
+// scrollContainer.addEventListener("wheel", (evt) => {
+//   evt.preventDefault();
+//   scrollContainer.scrollLeft += evt.deltaY;
+// });
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.scrollLeft += -900;
 });
