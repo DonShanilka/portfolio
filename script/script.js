fbe8994d435ebbebc49ff849bb9114ce90830fe7
Shanilka <shanilka799@gmail.com>
2024-04-25 12:54:46 +0530

    project page scrool js

--- script/script.js
+++ script/script.js
@@ -6,3 +6,13 @@ text.innerHTML = text.innerText.split("").map(
     `<span style="transform:rotate(${i * 9.6}deg)">${char}</span>`
 ).join("");
 
+
+// project page scrool js
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+});
