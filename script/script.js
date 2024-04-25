693319997b001395a3e9d6fc2439d0dbccfb4af1
Shanilka <shanilka799@gmail.com>
2024-04-25 12:47:06 +0530

    update css

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 // Home page js add
 const text = document.querySelector('.text p');
 text.innerHTML = text.innerText.split("").map(
- (char, i) =>
-  `<span style="transform:rotate(${i * 9.6}deg)">${char}</span>`
+  (char, i) =>
+    `<span style="transform:rotate(${i * 9.6}deg)">${char}</span>`
 ).join("");
+
