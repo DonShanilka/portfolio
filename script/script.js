247b741665a5b4bce11a3baf5a5d2fb97ea61ab7
Shanilka <shanilka799@gmail.com>
2024-06-10 00:27:02 +0530

    update css

--- script/script.js
+++ script/script.js
@@ -6,36 +6,6 @@ text.innerHTML = text.innerText.split("").map(
     `<span style="transform:rotate(${i * 9.6}deg)">${char}</span>`
 ).join("");
 
-// project page scrool js
-let scrollContainer = document.querySelector(".gallery");
-let backBtn = document.getElementById("backBtn");
-let nextBtn = document.getElementById("nextBtn");
-
-nextBtn.addEventListener("click", () => {
-  scrollContainer.style.scrollBehavior = "smooth";
-  scrollContainer.scrollLeft += 1400;
-});
-
-backBtn.addEventListener("click", () => {
-  scrollContainer.style.scrollBehavior = "smooth";
-  scrollContainer.scrollLeft += -1390;
-});
-
-// assingment page scroll js
-let scroll = document.querySelector(".assignment");
-let backBtnA = document.getElementById("backBtnA");
-let nextBtnA = document.getElementById("nextBtnA");
-
-nextBtnA.addEventListener("click", () => {
-  scroll.style.scrollBehavior = "smooth";
-  scroll.scrollLeft += 1400;
-});
-
-backBtnA.addEventListener("click", () => {
-  scroll.style.scrollBehavior = "smooth";
-  scroll.scrollLeft += -1391;
-});
-
 
 // Scroll Reveal Animation
 const sr = ScrollReveal({
@@ -48,55 +18,6 @@ const sr = ScrollReveal({
 sr.reveal('.full-name, .home-page-para', { origin: 'bottom' });
 
 
-// Work Page js
-$(function () {
-
-  window.sr = ScrollReveal();
-
-  if ($(window).width() < 768) {
-
-    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-    }
-
-    sr.reveal('.js--fadeInRight', {
-      origin: 'right',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
-    });
-
-  } else {
-
-    sr.reveal('.js--fadeInLeft', {
-      origin: 'left',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
-    });
-
-    sr.reveal('.js--fadeInRight', {
-      origin: 'right',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
-    });
-
-  }
-
-  sr.reveal('.js--fadeInLeft', {
-    origin: 'left',
-    distance: '300px',
-    easing: 'ease-in-out',
-    duration: 800,
-  });
-
-  sr.reveal('.js--fadeInRight', {
-    origin: 'right',
-    distance: '300px',
-    easing: 'ease-in-out',
-    duration: 800,
-  });
+//Animation Line js
 
 
-});
